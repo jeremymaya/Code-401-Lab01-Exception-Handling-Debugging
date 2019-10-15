c6649e60275546a8bb3e31a15740fc65b3cf1b15
Kyungrae Kim <jeremiah.kim@hotmail.com>
2019-10-15 09:39:13 -0700

    Main method completed with try/catch/finally code block

--- ExceptionHandlingDebugging/ExceptionHandlingDebugging/Program.cs
+++ ExceptionHandlingDebugging/ExceptionHandlingDebugging/Program.cs
@@ -4,9 +4,27 @@ namespace ExceptionHandlingDebugging
 {
     class Program
     {
+        // Main calls the StartSequence method in the try block
+        // If there is an error, catch block will display the Exception message
+        // Once the application is finshed, it tells user that the program is completed
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                StartSequence();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("It seems like there was an error.");
+                Console.WriteLine("");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("");
+                Console.WriteLine("Press 'Enter' to continue.");
+            }
+            finally
+            {
+                Console.WriteLine("Program is complete.");
+            }
         }
     }
 }
