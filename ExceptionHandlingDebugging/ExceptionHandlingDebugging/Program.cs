87cc8634aea883a1f37fd75d2456af5be65fdb61
Kyungrae Kim <jeremiah.kim@hotmail.com>
2019-10-15 14:08:12 -0700

    Populate method completed

--- ExceptionHandlingDebugging/ExceptionHandlingDebugging/Program.cs
+++ ExceptionHandlingDebugging/ExceptionHandlingDebugging/Program.cs
@@ -26,5 +26,44 @@ namespace ExceptionHandlingDebugging
                 Console.WriteLine("Program is complete.");
             }
         }
+        static void StartSequence()
+        {
+            Console.WriteLine("Welcome to my game! Let's do some math!");
+            Console.WriteLine("Please enter a number greater than zero");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+            int[] numbers = new int[number];
+            int[] populate = Populate(numbers);
+            int sum = GetSum(populate);
+            int product = GetProduct(populate, sum);
+            decimal quotient = GetQuotient(product);
+        }
+
+        static int[] Populate(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"Please enter number: {i + 1}/{arr.Length}");
+                string input = Console.ReadLine();
+                arr[i] = Convert.ToInt32(input);
+            }
+            return arr;
+        }
+
+        static int GetSum(int[] arr)
+        {
+            int num = 0;
+            return num;
+        }
+        static int GetProduct(int[] arr, int num)
+        {
+            return num;
+        }
+
+        static decimal GetQuotient(int num)
+        {
+            decimal deci = 0;
+            return deci;
+        }
     }
 }
